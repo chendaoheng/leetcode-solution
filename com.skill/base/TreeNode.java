5e15d25356f875d2fa83a34a625bab17aab08a42
chendaoheng <1023744525@qq.com>
2019-08-27 19:53:05 +0800

    添加题目常用的二叉树TreeNode类

---
+++ com.skill/base/TreeNode.java
@@ -0,0 +1,14 @@
+package base;
+
+/**
+ * <p>二叉树</p>
+ *
+ * @author chendaoheng
+ * @date 2019/8/27 19:49
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
