73a835fac12637df326c261c440fae36cd19a961
chendaoheng <1023744525@qq.com>
2019-09-18 15:47:48 +0800

    第1题【两数之和】-代码格式化

--- com.skill/algorithms/easy/Solution1.java
+++ com.skill/algorithms/easy/Solution1.java
@@ -25,8 +25,7 @@ public class Solution1 {
      */
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        v:
-        for (int i = 0; i < nums.length; i++) {
+        v:for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] + nums[i] == target) {
                     result[0] = i;
